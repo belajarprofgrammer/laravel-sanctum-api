20042f89b36943980dd476b746d1dcb27464131a
Belajar Profgrammer <belajarprofgrammer@gmail.com>
2025-09-12 18:31:16 +0700

    first commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Fluent;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    /**
+     * Register a new account.
+     */
+    public function register(RegisterRequest $request): JsonResponse
+    {
+        $user = new User();
+        $user->name = $request->validated('name');
+        $user->email = $request->validated('email');
+        $user->password = Hash::make($request->validated('password'));
+        $user->save();
+
+        return response()->json([
+            'code' => Response::HTTP_CREATED,
+            'status' => Response::$statusTexts[Response::HTTP_CREATED],
+            'data' => $user,
+        ], Response::HTTP_CREATED);
+    }
+
+    /**
+     * Login and return auth token.
+     */
+    public function login(LoginRequest $request): JsonResponse
+    {
+        if (Auth::attempt($request->validated())) {
+            $user = $request->user();
+
+            $accessToken = $user->createToken(
+                'access-token', ['*'], now()->addWeek(),
+            )->plainTextToken;
+
+            $token = new Fluent([
+                'access_token' => $accessToken,
+                'expired_at' => now()->addWeek()->timestamp,
+            ]);
+
+            return response()->json([
+                'code' => Response::HTTP_OK,
+                'status' => Response::$statusTexts[Response::HTTP_OK],
+                'data' => $token,
+            ], Response::HTTP_OK);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => __('auth.failed'),
+        ]);
+    }
+
+    /**
+     * Get current user.
+     */
+    public function me(Request $request): JsonResponse
+    {
+        $user = $request->user();
+
+        return response()->json([
+            'code' => Response::HTTP_OK,
+            'status' => Response::$statusTexts[Response::HTTP_OK],
+            'data' => $user,
+        ], Response::HTTP_OK);
+    }
+
+    /**
+     * Logout user and revoke tokens.
+     */
+    public function logout(Request $request): Response
+    {
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('auth')->group(function () {
+    Route::post('register', [AuthController::class, 'register'])
+        ->name('register');
+    Route::post('login', [AuthController::class, 'login'])
+        ->name('login');
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('me', [AuthController::class, 'me'])
+            ->name('me');
+        Route::delete('logout', [AuthController::class, 'logout'])
+            ->name('logout');
+    });
+});
