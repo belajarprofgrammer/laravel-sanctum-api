9bdb4fe49719fdb3769ca44d5f43e4c27d836d17
Belajar Profgrammer <belajarprofgrammer@gmail.com>
2025-09-24 11:35:40 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
